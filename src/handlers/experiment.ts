659f8e864d8605193754acb0da4593ae1e142997
Christopher Peerman <122806395+chris-dco@users.noreply.github.com>
2025-03-13 13:29:07 +0000

    SPT-1295: Added API Gateway config and basic page
    Basic page working. Assets uploaded to S3 and page being loaded using
    Lambda.

--- src/handlers/experiment.ts
+++ src/handlers/experiment.ts
@@ -7,13 +7,28 @@ nunjucks.configure([
 
 const sampleTemplate = `
     {% extends "govuk/template.njk" %}
+    {% block head %}
+      <link rel="stylesheet" href="{{ assetPath | default("/assets", true) }}/../stylesheets/govuk-frontend-5.9.0.min.css">
+    {% endblock %}
+    {% block content %}
+    <h1 class="govuk-heading-xl">Hello world</h1>
+    {% endblock %}
+
+    {% block bodyEnd %}
+    {# Run JavaScript at end of the <body>, to avoid blocking the initial render. #}
+    <script type="module" src="{{ assetPath | default("/assets", true) }}/javascripts/govuk-frontend-5.9.0.min.js"></script>
+    <script type="module">
+        import { initAll } from '{{ assetPath | default("/assets", true) }}/javascripts/govuk-frontend-5.9.0.min.js'
+        initAll()
+    </script>
+    {% endblock %}
     `;
 
 export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     try {
         return {
             statusCode: 200,
-            body: nunjucks.renderString(sampleTemplate, { assetPath: "https://design-system.service.gov.uk/assets" }),
+            body: nunjucks.renderString(sampleTemplate, { assetPath: "Stage/assets" }),
             headers: {
                 "content-type": "text/html"
             }
