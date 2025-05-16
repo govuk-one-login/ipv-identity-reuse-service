abd57237a26fe35cf38b2c27797c9dff572a2543
Christopher Peerman <122806395+chris-dco@users.noreply.github.com>
2025-05-16 14:00:53 +0100

    SPT-1449: Added nunjucks support to AIS Producer
    * Converted ais-producer-stub-handler to a nunjucks application
      * Now outputs an HTML page using the govuk-frontend
      * Basic template to output the Hello world message
    * Migrated to Makefile rather than esbuild target so that the final
      binary contained the govuk-frontend.
    * Updated Cloud Formation to only deploy AIS Producer in dev, build and
      staging.

--- src/ais-producer-stub/ais-producer-stub-handler.ts
+++ src/ais-producer-stub/ais-producer-stub-handler.ts
@@ -1,1 +1,19 @@
-export const handler = async (): Promise<string> => "Hello, world!";
+import type { APIGatewayProxyResult } from "aws-lambda";
+import nunjucks from "nunjucks";
+import sampleTemplate from "./index.njk";
+import fs from "fs";
+
+nunjucks.configure(["./govuk-frontend/dist"]);
+
+const sampleTemplateContents = fs.readFileSync(sampleTemplate, "utf8");
+
+export const handler = async (): Promise<APIGatewayProxyResult> => ({
+  statusCode: 200,
+  body: nunjucks.renderString(sampleTemplateContents, {
+    assetPath: "v1/assets",
+    rootPath: "v1",
+  }),
+  headers: {
+    "content-type": "text/html",
+  },
+});
