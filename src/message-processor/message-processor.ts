452b7489e75a6e7e124e60c08463d69a5597a9a6
Sam Leen <sam.leen@digital.cabinet-office.gov.uk>
2025-06-24 14:20:53 +0100

    SPT-1406: Log metrics in `message-processor` lambda
    - Add powertools metrics dependency.
    - Log how many messages have been sento into lambda.
    - Assert that requests have to have log ids.

--- src/message-processor/message-processor.ts
+++ src/message-processor/message-processor.ts
@@ -1,7 +1,17 @@
+import { logIds } from "../types/logIds";
 import logger from "../utils/logger";
+import { Metrics, MetricUnit } from "@aws-lambda-powertools/metrics";
 
-export const handler = async (request: any): Promise<string> => {
-  logger.info(request);
+const messageProcessorMetrics = new Metrics();
+const MESSAGES_RECEIVED = "MessagesRecieved";
 
+export interface Request {
+  logIds: logIds;
+}
+
+export const handler = async <T extends Request>(request: T): Promise<string> => {
+  logger.info(JSON.stringify(request.logIds));
+  messageProcessorMetrics.addMetric(MESSAGES_RECEIVED, MetricUnit.Count, 1);
+  messageProcessorMetrics.publishStoredMetrics();
   return "execution succeeded";
 };
---
+++ src/types/logIds.ts
@@ -0,0 +1,7 @@
+export type logIds = {
+  session_id: string;
+  client_id: string;
+  persistent_session_id: string;
+  client_session_id: string;
+  request_id: string;
+};
