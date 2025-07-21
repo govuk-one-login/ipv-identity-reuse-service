f7e71f9ba5d540a4d3d215a5b4772b6ddd248ebe
Christopher Peerman <122806395+chris-dco@users.noreply.github.com>
2025-07-21 14:08:27 +0100

    SPT-1410: Fixed mock cucumber tests failing
    Fixed mock cucumber tests failing due to ESM and removal of
    ExampleLambda.

--- tests/acceptance/steps/example-lambda.step.ts
+++ tests/acceptance/steps/example-lambda.step.ts
@@ -27,11 +27,12 @@ When<TestWorld>("I call the Lambda", async function () {
     throw new Error("Lambda Physical Id is not defined");
   }
 
-  this.lambdaResponse = await this.lambdaTest.callLambda(this.lambdaPhysicalId);
+  this.lambdaResponse = await this.lambdaTest.callLambda(this.lambdaPhysicalId, '{"Records":[]}');
 });
 
-Then<TestWorld>("it will return the string {string}", function (expected: string) {
+Then<TestWorld>("it will return null", function () {
   assert.equal(this.lambdaResponse?.StatusCode, 200);
-  assert.ok(this.lambdaResponse?.Payload);
-  assert.equal(Buffer.from(this.lambdaResponse.Payload).toString(), expected);
+  assert.equal(new TextDecoder().decode(this.lambdaResponse?.Payload), "null");
 });
+
+export {};
