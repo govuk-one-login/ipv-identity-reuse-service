12b55abc196e8d44a22efa27b67d76d6a3a74dd7
Sam Leen <sam.leen@digital.cabinet-office.gov.uk>
2025-09-12 11:25:44 +0100

    SPT-1513: Amend `user-identity` POST feature tests
    - Amend acceptance tests to verify usage of bearer token.
    - Add method to generate bearer tokens for test purposes.
    - Add `EVCS_API_KEY_SECRET_ARN` to lambda environment.

--- tests/acceptance/steps/user-identity.step.ts
+++ tests/acceptance/steps/user-identity.step.ts
@@ -1,21 +1,18 @@
 import { Given, When, Then, Before } from "@cucumber/cucumber";
 import { type InvokeCommandOutput } from "@aws-sdk/client-lambda";
 import assert from "node:assert";
-import { UserIdentityInput } from "../../../src/types/interfaces";
-import { HttpCodesEnum } from "../../../src/types/constants";
+import { StoredIdentityResponse } from "../../../src/types/interfaces";
 import request from "supertest";
 import { LambdaTestClient } from "./utils/lambda-test-client";
 import EndPoints from "./utils/endpoints";
 import { getApiKey } from "./utils/get-api-key";
+import { getBearerToken } from "./utils/get-bearer-token";
 interface TestWorld {
   lambdaTest: LambdaTestClient;
   lambdaPhysicalId?: string;
   lambdaResponse?: InvokeCommandOutput;
 }
-const userIdentityInput: UserIdentityInput = {
-  govukSigninJourneyId: "j8mMnXW_rP6JqNXBKKf8xsGXttk121",
-  vtr: ["P1", "P3"],
-};
+
 Before<TestWorld>(function () {
   const stackName = process.env.SAM_STACK_NAME;
   if (typeof stackName != "string" || stackName.length === 0) {
@@ -25,49 +22,52 @@ Before<TestWorld>(function () {
   this.lambdaTest = new LambdaTestClient(stackName);
 });
 type WorldDefinition = {
-  testUserId: string;
+  bearerToken: string;
   userIdentityPOSTResponse: import("superagent/lib/node/response.js");
+  userIdentityGETResponse: import("superagent/lib/node/response.js");
 };
 
 Given<TestWorld>("I have the Lambda with resource name {string}", async function (lambdaName: string) {
   this.lambdaPhysicalId = await this.lambdaTest.getPhysicalResourceId(lambdaName);
 });
-Given("I send a POST request with input value", async function (this: WorldDefinition) {
+
+Given("I send a POST request", async function (this: WorldDefinition) {
   this.userIdentityPOSTResponse = await request(EndPoints.BASE_URL as string)
     .post(EndPoints.PATH_USER_IDENTITY)
-    .send(userIdentityInput)
     .set("x-api-key", await getApiKey())
-    .set("authorization", "Bearer 123")
+    .set("authorization", this.bearerToken)
     .set("Content-Type", "application/json")
     .set("Accept", "*/*");
-});
-Given("I send a POST request with malformed data", async function (this: WorldDefinition) {
-  this.userIdentityPOSTResponse = await request(EndPoints.BASE_URL as string)
-    .post(EndPoints.PATH_USER_IDENTITY)
-    .send("")
-    .set("authorization", "a-dummy-access-token")
-    .set("x-api-key", await getApiKey())
-    .set("Content-Type", "application/json")
-    .set("Accept", "*/*");
-});
-Given("I send a POST request without authorization header", async function (this: WorldDefinition) {
-  this.userIdentityPOSTResponse = await request(EndPoints.BASE_URL as string)
-    .post(EndPoints.PATH_USER_IDENTITY)
-    .send(userIdentityInput)
-    .set("x-api-key", await getApiKey())
-    .set("Content-Type", "application/json")
-    .set("Accept", "*/*");
-});
-Then("I should receive a success response", async function () {
-  assert.equal(this.userIdentityPOSTResponse.statusCode, HttpCodesEnum.OK);
-});
-Then("I should receive a Bad Request", async function () {
-  assert.equal(this.userIdentityPOSTResponse.status, HttpCodesEnum.BAD_REQUEST);
 });
 
-Then("I should receive Unauthorized", async function () {
-  assert.equal(this.userIdentityPOSTResponse.status, HttpCodesEnum.UNAUTHORIZED);
+Given(/a (.*) bearer token/, async function (this: WorldDefinition, status: string) {
+  let token: string;
+
+  switch (status) {
+    case "valid":
+      token = await getBearerToken("TEST_USER");
+      break;
+    case "bad":
+      token = "Bearer a.bad.jwt";
+      break;
+    case "absentee":
+      token = await getBearerToken("NO_USER");
+      break;
+    default:
+      throw new Error("Unsupported token type.");
+  }
+
+  this.bearerToken = token;
 });
+
+Then("the status code should be {int}", async function (statusCode: number) {
+  assert.equal(this.userIdentityPOSTResponse.statusCode, statusCode);
+});
+
+Then("The stored identity should be returned", function (this: WorldDefinition) {
+  JSON.parse(this.userIdentityPOSTResponse.body) satisfies StoredIdentityResponse;
+});
+
 When<TestWorld>("I call the Lambda", async function () {
   if (!this.lambdaPhysicalId) {
     throw new Error("Lambda Physical Id is not defined");
