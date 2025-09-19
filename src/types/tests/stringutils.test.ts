6d0d7cdef88eb70e5d8f3414d9c49406ed2aca85
Michaela Benyohai <michaela.benyohai@digital.cabinet-office.gov.uk>
2025-09-19 11:33:40 +0100

    SPT-1635 Silently handle AIS events that are missing intervention_code
    Do not throw an error when an AIS event does not contain the
    intervention_code field. Instead silently ignore it.
    
    We were expecting an AIS event to always contain an intervention_code
    field and throwing an exception when this field was missing. However,
    it turns out that this is an optional field and we are receiving AIS
    events in production without this field, leading to an error to be
    thrown and messages ending up on the DLQ. This commit fixes this issue.

---
+++ src/types/tests/stringutils.test.ts
@@ -0,0 +1,20 @@
+import { isStringWithLength } from "../stringutils";
+
+describe("isStringWithLength", () => {
+  it("should return false if value is undefined", () => {
+    expect(isStringWithLength(undefined)).toEqual(false);
+  });
+
+  it("should return false if value is empty string", () => {
+    expect(isStringWithLength("")).toEqual(false);
+  });
+
+  it("should return false for value that is not a string type", () => {
+    expect(isStringWithLength(100)).toEqual(false);
+    expect(isStringWithLength({})).toEqual(false);
+  });
+
+  it("should return true if value is a string with length", () => {
+    expect(isStringWithLength("hello")).toEqual(true);
+  });
+});
---
+++ src/types/tests/txmaMessage.test.ts
@@ -0,0 +1,61 @@
+import { isTxmaMessage } from "../txmaMessage";
+
+describe("isTxmaMessage", () => {
+  it("should return true when required fields present", () => {
+    expect(
+      isTxmaMessage({
+        user_id: "jane.smith-12345",
+        timestamp: 1752755454,
+        intervention_code: "12",
+      })
+    ).toEqual(true);
+  });
+
+  it("should return true when extra fields present", () => {
+    expect(
+      isTxmaMessage({
+        user_id: "jane.smith-12345",
+        timestamp: 1752755454,
+        intervention_code: "12",
+        event_id: 345,
+      })
+    ).toEqual(true);
+  });
+
+  it("should return false when user_id missing", () => {
+    expect(
+      isTxmaMessage({
+        timestamp: 1752755454,
+        intervention_code: "12",
+      })
+    ).toEqual(false);
+  });
+
+  it("should return false when timestamp missing", () => {
+    expect(
+      isTxmaMessage({
+        user_id: "jane.smith-12345",
+        intervention_code: "12",
+      })
+    ).toEqual(false);
+  });
+
+  it("should return true when intervention_code missing", () => {
+    expect(
+      isTxmaMessage({
+        user_id: "jane.smith-12345",
+        timestamp: 1752755454,
+      })
+    ).toEqual(true);
+  });
+
+  it("should return false when user_id empty", () => {
+    expect(
+      isTxmaMessage({
+        user_id: "",
+        timestamp: 1752755454,
+        intervention_code: "12",
+      })
+    ).toEqual(false);
+  });
+});
