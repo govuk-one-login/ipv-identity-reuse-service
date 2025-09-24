cb8dc4ae1542fa591aa327aff05f848fef1fe4ce
chaitan Guttarlapalli <chaitan.guttarlapalli@digital.cabinet-office.gov.uk>
2025-09-24 15:41:44 +0100

    SPT-1603:Implement DID using lambda
    - generate did using lambda functions
    - Changes for endpoint

---
+++ src/handlers/did-generation-handler.ts
@@ -0,0 +1,26 @@
+import logger from "../commons/logger";
+import { DIDDocument } from "did-resolver";
+import { getKmsPublicKey } from "../services/did-generation-service";
+import { APIGatewayProxyResult } from "aws-lambda";
+
+import { binaryPkToJwk, updateJwk } from "../utils/helper";
+export const handler = async (): Promise<APIGatewayProxyResult> => {
+  logger.debug("Received message");
+  const alg = "ES256";
+  const didSigningKey = process.env.DID_SIGNING_KEY_ARN;
+  const publicKeyData = await getKmsPublicKey(didSigningKey!);
+  let jwk = await binaryPkToJwk(publicKeyData.publicKey!, alg);
+  jwk = updateJwk(jwk, { alg });
+  const didDocument: DIDDocument = {
+    id: publicKeyData.keyId,
+    assertionMethod: [
+      {
+        type: "JsonWebKey",
+        id: "did:web:identity.dev.account.gov.uk#" + publicKeyData.keyId,
+        controller: "did:web:identity.dev.account.gov.uk",
+        publicKeyJwk: jwk,
+      },
+    ],
+  };
+  return { statusCode: 200, body: JSON.stringify(didDocument)};
+};
