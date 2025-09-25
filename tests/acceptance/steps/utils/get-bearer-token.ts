275fcb8f798ee1b1c03478048232d273e583bb44
Christopher Peerman <122806395+chris-dco@users.noreply.github.com>
2025-09-25 15:43:46 +0100

    SPT-1636: Configured to use mock credential svc
    Configured to use mock credential service and configured acceptance test
    to run in pipeline.

--- tests/acceptance/steps/utils/get-bearer-token.ts
+++ tests/acceptance/steps/utils/get-bearer-token.ts
@@ -1,14 +1,17 @@
+const getMockCredentialStorePath = () => {
+  return process.env.ENVIRONMENT === "local" || process.env.ENVIRONMENT === "dev"
+    ? "credential-store.reuse.dev.stubs.account.gov.uk"
+    : "credential-store.reuse.stubs.account.gov.uk";
+};
+
 export const getBearerToken = async (sub: string): Promise<string> => {
-  const response = await fetch(
-    `https://mock.credential-store.${process.env.ENVIRONMENT || "dev"}.account.gov.uk/generate`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ sub }),
-    }
-  );
+  const response = await fetch(`https://${getMockCredentialStorePath()}/generate`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ sub }),
+  });
 
   if (!response.ok) {
     throw new Error(`Request failed with status ${response.status}`);
