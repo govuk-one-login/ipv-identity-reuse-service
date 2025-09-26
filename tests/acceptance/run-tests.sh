a20cbb43038f8410124ac7a45c9ab96310c1d99c
Chris Clayson <chris.clayson@digital.cabinet-office.gov.uk>
2025-09-26 11:59:48 +0100

    SPT-1621: Add verbosity to test image & ensure working directory
    - Add some `echo` statement to show progress of `run-tests.sh`
    - Add parameter to ensure Cucumber echos test progress
    - Add `cd /app` prior to running the tests to ensure tests are run
      from the correct location (`WORKDIR` may get ignored in the when
      running in the pipeline).

--- tests/acceptance/run-tests.sh
+++ tests/acceptance/run-tests.sh
@@ -2,12 +2,17 @@
 TEST_REPORT_ABSOLUTE_DIR="${TEST_REPORT_ABSOLUTE_DIR:-/app/reports}"
 
 if [ ! -d "${TEST_REPORT_ABSOLUTE_DIR}" ]; then
+  echo "Creating directory ${TEST_REPORT_ABSOLUTE_DIR}"
   mkdir "${TEST_REPORT_ABSOLUTE_DIR}"
 fi
 
+echo "Test reports will be written to ${TEST_REPORT_ABSOLUTE_DIR}"
+
+cd /app
 echo Running acceptance tests...
 ./node_modules/.bin/cucumber-js \
   --require './dist/tests/acceptance/steps/*.js' \
   --format "json:${TEST_REPORT_ABSOLUTE_DIR}/cucumber.json" \
+  --format pretty \
   --tags 'not @ignore' \
   ./features
