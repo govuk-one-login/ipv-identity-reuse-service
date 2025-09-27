890027b9c39c464de79e8cbba0db813a204af348
Michaela Benyohai <michaela.benyohai@digital.cabinet-office.gov.uk>
2025-09-27 22:16:38 +0100

    SPT-1514 Create function for getting CURRENT verifiable credentials
    The reusable identity will be based on verifiable credentials from
    storage that have `state` "CURRENT". This function filters for those
    from the EVCS response and decodes the JWTs into objects with
    data-vocab types.

---
+++ src/credential-store/tests/encrypted-credential-store.test.ts
@@ -0,0 +1,46 @@
+import { CredentialStoreIdentityResponse, JWTIncludingStateAndMetadata } from "../credential-store-identity-response";
+import { IdentityCheckCredentialJWTClass } from "@govuk-one-login/data-vocab/credentials";
+import { getDefaultStoredIdentityHeader, sign } from "../../../tests/acceptance/steps/utils/jwt-utils";
+import { parseCurrentVerifiableCredentials } from "../encrypted-credential-store";
+
+describe("parseCurrentVerifiableCredentials", () => {
+  it("should return verifiable credentials with CURRENT state only", () => {
+    const identityResponse: CredentialStoreIdentityResponse = {
+      si: {
+        state: "doesThisActuallyHaveState",
+        vc: "jwtString",
+        metadata: null,
+      },
+      vcs: [
+        createVerifiableCredentialWithState("iss1", "CURRENT"),
+        createVerifiableCredentialWithState("iss2", "CURRENT"),
+        createVerifiableCredentialWithState("iss3", "HISTORIC"),
+        createVerifiableCredentialWithState("iss4", "PENDING_RETURN"),
+      ],
+    };
+
+    const result = parseCurrentVerifiableCredentials(identityResponse);
+    expect(result).toHaveLength(2);
+    expect(result.map((vc) => vc.iss)).toContain("iss1");
+    expect(result.map((vc) => vc.iss)).toContain("iss2");
+  });
+});
+
+const createVerifiableCredentialWithState = (issuer: string, state: string): JWTIncludingStateAndMetadata => {
+  return {
+    state: state,
+    vc: sign(getDefaultStoredIdentityHeader(), getVerifiableCredential(issuer)),
+    metadata: null,
+  };
+};
+
+const getVerifiableCredential = (issuer: string): IdentityCheckCredentialJWTClass => {
+  return {
+    iss: issuer,
+    nbf: 1234,
+    sub: "user1234",
+    vc: {
+      evidence: [],
+    },
+  };
+};
--- tests/acceptance/steps/utils/jwt-utils.ts
+++ tests/acceptance/steps/utils/jwt-utils.ts
@@ -1,7 +1,8 @@
 import { base64url, JWTHeaderParameters, JWTPayload } from "jose";
 import { randomString } from "./string-utils";
+import { JWTClass } from "@govuk-one-login/data-vocab/credentials";
 
-export const sign = (jwtHeader: JWTHeaderParameters, jwtPayload: JWTPayload): string => {
+export const sign = (jwtHeader: JWTHeaderParameters, jwtPayload: JWTPayload | JWTClass): string => {
   const header = base64url.encode(Buffer.from(JSON.stringify(jwtHeader)));
   const payload = base64url.encode(Buffer.from(JSON.stringify(jwtPayload)));
   return `${header}.${payload}.`;
