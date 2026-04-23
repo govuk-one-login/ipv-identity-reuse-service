aaa5aa04e0555b6d597348d7c916c211b409af10
mislam246 <muhammed.islam@digital.cabinet-office.gov.uk>
2026-04-23 13:09:52 +0100

    SPT-1903: Added submit logic
    - Added logic to route user back to the provided redirect URI,
    passing the auth code and state in the query params
    - Added tests

---
+++ src/handlers/confirm-details/tests/confirm-details-submission-handler.test.ts
@@ -0,0 +1,45 @@
+import { APIGatewayEventRequestContextWithAuthorizer, APIGatewayProxyEvent } from "aws-lambda";
+import { expect, it } from "vitest";
+import { lambdaHandler } from "../confirm-details-submission-handler";
+
+it("should return a 302 status code on a successful request", async () => {
+  const event = createMockAPIGatewayProxyEvent({});
+
+  const response = await lambdaHandler(event);
+  expect(response).toStrictEqual({
+    statusCode: 302,
+    body: "",
+    headers: {
+      Location: "https://api.example.com/?code=abc123&state=test-state-id",
+    },
+  });
+});
+
+it("should return 302 status code on a successful request and handle URL encoded redirect_uri", async () => {
+  const event = createMockAPIGatewayProxyEvent({});
+
+  const response = await lambdaHandler(event);
+  expect(response).toStrictEqual({
+    statusCode: 302,
+    body: "",
+    headers: {
+      Location: "https://api.example.com/?code=abc123&state=test-state-id",
+    },
+  });
+});
+
+const createMockAPIGatewayProxyEvent = (event: Partial<APIGatewayProxyEvent>): APIGatewayProxyEvent => ({
+  body: "redirectUri=https%3A%2F%2Fapi.example.com&code=abc123&state=test-state-id",
+  headers: {},
+  multiValueHeaders: {},
+  httpMethod: "POST",
+  isBase64Encoded: false,
+  path: "/",
+  pathParameters: null,
+  queryStringParameters: null,
+  multiValueQueryStringParameters: null,
+  stageVariables: null,
+  requestContext: {} as APIGatewayEventRequestContextWithAuthorizer<never>,
+  resource: "/",
+  ...event,
+});
