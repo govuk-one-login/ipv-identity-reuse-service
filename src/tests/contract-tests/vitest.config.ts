2db9f0efdc6872fdfefd8ef2df66d7f8d0195920
Christopher Peerman <122806395+chris-dco@users.noreply.github.com>
2026-05-11 15:24:44 +0100

    BAU: Added retry login for consumer tests
    This is due to communications issues that we have observed between the
    RUST PACT server and the PACT client. It appears that the PACT server
    handles the request, however the PACT client does not receive the
    expected response. We have compared the logs between a working test run
    and a failed test run and there are identical leading us to assume that
    there are comms issues.

--- src/tests/contract-tests/vitest.config.ts
+++ src/tests/contract-tests/vitest.config.ts
@@ -4,5 +4,10 @@ export default defineConfig({
   test: {
     projects: ["src/tests/contract-tests/consumer", "src/tests/contract-tests/provider"],
     bail: 1,
+    retry: {
+      // Retry to get around comms issues between PACT RUST server and JS client
+      count: 2,
+      delay: 50,
+    },
   },
 });
