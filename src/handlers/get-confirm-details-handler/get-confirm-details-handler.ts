d21c1a7d8c4df6e2feab5339424b3cf54487a98f
Michaela Benyohai <michaela.benyohai@digital.cabinet-office.gov.uk>
2026-09-18 17:43:09 +0100

    SPT-2024: Move validate-records.ts to domain/stored-identity dir
    Also rename the file to stored-identity-validator.ts. This file contains
    code for valdiating a stored identity and therefore fits in this new
    directory.

--- src/handlers/get-confirm-details-handler/get-confirm-details-handler.ts
+++ src/handlers/get-confirm-details-handler/get-confirm-details-handler.ts
@@ -4,23 +4,25 @@ import path from "node:path";
 import logger from "../../commons/logger.js";
 import mainPageTemplate from "./index.njk";
 import { getCookieValues } from "../../commons/cookie-utilities.js";
-import { handleGetIdentityFromCredentialStore, validateIdentityRecords } from "../../commons/validate-records.js";
+import {
+  handleGetIdentityFromCredentialStore,
+  validateStoredIdentity,
+} from "../../domain/stored-identity/stored-identity-validator.js";
 import { getSessionDetails } from "../../api/oauth-internal-api.js";
 import { redirectToErrorPage } from "../../api/sis-api.js";
 import { EVCSError, StoredIdentityValidationError } from "../../commons/errors.js";
 import { HttpCodesEnum } from "../../commons/constants.js";
 import { extractUserDetails } from "./user-details-content.js";
 import translations from "../../../locales/en/translation.json" with { type: "json" };
-
 import { getConfiguration } from "../../commons/configuration.js";
 import { parseCurrentVerifiableCredentials, EVCSIdentityResponse } from "../../api/evcs-api.js";
 import { hasIdentityExpired } from "../../identity-reuse/identity-expiry-service.js";
 import { calculateVot } from "../../identity-reuse/calculate-vot.js";
-import { StoredIdentityJWT } from "../post-phase2-user-identity-handler/stored-identity-jwt.js";
 import { getJwtBody } from "../../commons/jwt-utilities.js";
 import { Metrics } from "@aws-lambda-powertools/metrics";
 import { MetricDimension, MetricName } from "../../commons/metric-enum.js";
 import { IdentityVectorOfTrust } from "@govuk-one-login/data-vocab/credentials.js";
+import { StoredIdentityJWT } from "../../domain/stored-identity/stored-identity-types.js";
 
 const govukFrontendDistribution = path.join(path.dirname(require.resolve("govuk-frontend/package.json")), "dist");
 const nunjucksEnvironment = nunjucks.configure([
@@ -101,7 +103,7 @@ export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGat
     }
 
     const identityResponse = await handleGetIdentityFromCredentialStore(`Bearer ${storageAccessToken}`, subject);
-    const { kidValid, signatureValid, isValid, storedIdentityJwt } = await validateIdentityRecords(identityResponse);
+    const { kidValid, signatureValid, isValid, storedIdentityJwt } = await validateStoredIdentity(identityResponse);
 
     if (!kidValid || !signatureValid || !isValid || !storedIdentityJwt) {
       logger.error("Record validation failed for existing user", { kidValid, signatureValid, isValid });
